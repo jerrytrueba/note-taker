c138c0f2f16d35bf4e728d6a34022ebb475ef900
Jerry Trueba <jerrytrueba10@gmail.com>
2022-06-08 20:28:01 -0700

    updated

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# note-taker
+## Note Taker
+
+## Description
+For this project, we had to create a Note Taker using express.js, node.js, and Heroku. We have been provided with some starter code for the front end of the application and I was tasked with completing the back end of the code.
+
+## Screenshot of the Live Note Taker Application
+
+
+## Applications Used
+1. Express
+2. Node
+3. Heroku
+
+## Questions/Comments
+jerrytrueba10@gmail.com
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const PORT = PORT || 3001;
+const app = express();
+const uuid = require('uuid');
+const allNotes = require('./db/db.json');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});

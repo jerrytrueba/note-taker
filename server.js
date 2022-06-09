24d1fde978235b638fe61acb18e4835b9a3215dd
Jerry Trueba <jerrytrueba10@gmail.com>
2022-06-08 22:18:29 -0700

    updated

--- server.js
+++ server.js
@@ -1,68 +1,58 @@
-const express = require('express');
 const path = require('path');
-const fs = require('fs');
 const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+const express = require('express');
+const {v4:uuidv4} = require('uuid');
+
 const app = express();
-const uuid = require('uuid');
-const allNotes = require('./db/db.json');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) throw err;
-        res.json(JSON.parse(data))
-    })
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data))
+  })
 
 });
 
-function createNewNote(body, notesArray) {
-    const newNote = body;
-    if (!Array.isArray(notesArray))
-        notesArray = [];
-
-    if (notesArray.length === 0)
-        notesArray.push(0);
-
-    body.id = notesArray[0];
-    notesArray[0]++;
-
-    notesArray.push(newNote);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify(notesArray, null, 2)
-    );
-    return newNote;
-}
+app.get('/notes', (req, res) => {
+  console.log(path.join(__dirname, '/public/notes.html'));
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
 
 app.post('/api/notes', (req, res) => {
-    const newNote = createNewNote(req.body, allNotes);
-    res.json(newNote);
-});
-
-function deleteNote(id, notesArray) {
-    for (let i = 0; i < notesArray.length; i++) {
-        let note = notesArray[i];
-
-        if (note.id == id) {
-            notesArray.splice(i, 1);
-            fs.writeFileSync(
-                path.join(__dirname, './db/db.json'),
-                JSON.stringify(notesArray, null, 2)
-            );
-
-            break;
-        }
-    }
-}
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    let newNote = req.body;
+    newNote.id= uuidv4()
+    let notes = JSON.parse(data)
+    notes.push(newNote);
+  
+  fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+    if (err) throw err;
+    res.json(newNote)
+  })
+  })
+})
 
 app.delete('/api/notes/:id', (req, res) => {
-    deleteNote(req.params.id, allNotes);
-    res.json(true);
-});
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    
+    let notes = JSON.parse(data);
+    let filterNotes = notes.filter(note => note.id !== req.params.id)
+    
+  
+  fs.writeFile('./db/db.json', JSON.stringify(filterNotes), (err) => {
+    if (err) throw err;
+    res.json(filterNotes)
+  })
+  })
+})
 
 app.listen(PORT, () => {
-    console.log(`App in http://localhost:${PORT}`);
+  console.log(`App at http://localhost:${PORT}`);
 });
